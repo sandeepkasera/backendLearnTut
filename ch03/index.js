3bf3a2a2a5e81440573470f150f46a316079fb45
Sandeep Kumar <sandeepkasera>
2024-03-05 16:30:12 +0530

    ch03

---
+++ ch03/index.js
@@ -0,0 +1,17 @@
+const { format } = require('date-fns');
+
+const { v4: uuid } = require('uuid')
+
+
+
+console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
+
+
+
+console.log('HI');
+
+console.log(uuid());
+
+
+
+console.log(uuid());
